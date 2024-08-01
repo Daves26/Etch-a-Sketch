f9d84d68c5aefb6aee7024840a8638463ba3ce9e
Daves26 <juandavesga8@gmail.com>
2024-08-01 18:16:47 -0500

    Variable grid/canvas size

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
 const container = document.querySelector(".container");
 const square = document.createElement("div");
 square.classList.add("square");
-let size = 16;
+const max_size = 100;
+let size = 50;
 
-for (let i = 0; i < size**2; i++) {
-    container.appendChild(square.cloneNode(true));
+function createCanvas(size) {
+    let height = 100 / size;
+    square.style.height = height + "%";
+    for (let i = 0; i < size**2; i++) {
+        container.appendChild(square.cloneNode(true));
+    }
+    enableDrawing();
+    return;
+}
+createCanvas(size);
+
+// Drawing effect
+function enableDrawing() {
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((sqr) => {
+        sqr.addEventListener("mouseover", () => {
+            sqr.style.backgroundColor = "black";
+        });
+        sqr.addEventListener("mouseout", () => {
+            sqr.style.backgroundColor = "#4d4d4d";
+            sqr.style.filter = "blur(7px)";
+        });
+    });
 }
 
-const squares = document.querySelectorAll(".square");
-
-squares.forEach((sqr) => {
-    sqr.addEventListener("mouseover", () => {
-        sqr.style.backgroundColor = "black";
-    });
-    sqr.addEventListener("mouseout", () => {
-        sqr.style.backgroundColor = "#2e2e2e";
-        sqr.style.filter = "blur(3px)";
-    });
-});
-
+// Resize canvas/grid
 const button = document.querySelector("#size");
 button.addEventListener("click", () => {
-    size = prompt("Enter new canvas size\n(squares per side)");
+    size = 101;
+    while(size > max_size) {
+        size = prompt("Enter new canvas size\n(squares per side)(max. 100)");
+    }
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+    createCanvas(size);
 });
+
